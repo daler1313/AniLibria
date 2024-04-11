6c34b734ac0fe7aebf7fd7c0382597479eda4716
daler1313 <dfozilov031@gmail.com>
2024-04-11 13:52:09 +0500

    ошибка

--- ApiAniLibria/Program.cs
+++ ApiAniLibria/Program.cs
@@ -4,10 +4,9 @@ using Application;
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +16,6 @@ builder.Services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
